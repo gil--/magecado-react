99e456ea5e381130768685a9c89c8fe768c51d19
Gil Greenberg <gilgreenberg@gmail.com>
2016-01-03 03:32:04 -0500

    Add proper markdown rendering and integrate views

--- src/components/Snippet/index.js
+++ src/components/Snippet/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { Router, Route, Link, IndexRoute, IndexLink} from 'react-router';
-import InlineSvg from 'svg-inline-react';
-import Remarkable from 'remarkable';
+import InlineSvg from 'svg-inline-react'; //inline svg
+import Remarkable from 'remarkable'; //markdown rendering
+import hljs from 'highlight.js'; //syntax highlighting
+var moment = require('moment'); // For dates
 
 // Components
 import AddSnippet from '../AddSnippet';
@@ -16,6 +18,7 @@ import AddSnippet from '../AddSnippet';
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var Snippet = React.createClass({
   renderPagination : function() {
+    var snippetPager = {previous: null, next: null, random: null};
     var nextSnippet = null;
     var previousSnippet = null;
     var randomSnippet = null;
@@ -98,37 +101,53 @@ var Snippet = React.createClass({
   render : function() {
     // Core Snippet
     var snippet = this.props.info;
+
     var md = new Remarkable('full', {
-  html:         false,        // Enable HTML tags in source
-  xhtmlOut:     false,        // Use '/' to close single tags (<br />)
-  breaks:       false,        // Convert '\n' in paragraphs into <br>
-  langPrefix:   'language-',  // CSS language prefix for fenced blocks
-  linkify:      true,         // autoconvert URL-like texts to links
+      html:         true,        // Enable HTML tags in source
+      xhtmlOut:     true,        // Use '/' to close single tags (<br />)
+      breaks:       true,        // Convert '\n' in paragraphs into <br>
+      langPrefix:   'language-',  // CSS language prefix for fenced blocks
+      linkify:      true,         // autoconvert URL-like texts to links
 
-  // Enable some language-neutral replacements + quotes beautification
-  typographer:  false,
+      // Enable some language-neutral replacements + quotes beautification
+      typographer:  false,
 
-  // Double + single quotes replacement pairs, when typographer enabled,
-  // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
-  quotes: '“”‘’',
+      // Double + single quotes replacement pairs, when typographer enabled,
+      // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+      quotes: '“”‘’',
 
-  // Highlighter function. Should return escaped HTML,
-  // or '' if input not changed
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(lang, str).value;
-      } catch (__) {}
+      // Highlighter function. Should return escaped HTML,
+      // or '' if input not changed
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (__) {}
+        }
+
+        try {
+          return hljs.highlightAuto(str).value;
+        } catch (__) {}
+
+        return ''; // use external default escaping
+      }
+    });
+
+    var snippetCode = md.render(snippet.snippetCode);
+
+    var snippetTags = null;
+
+    if(snippet.tags) {
+      snippetTags = snippet.tags.map(function(tag, tagIndex) {
+        return <li key={tagIndex}>{tag}</li>;
+      });
     }
 
-    try {
-      return hljs.highlightAuto(str).value;
-    } catch (__) {}
-
-    return ''; // use external default escaping
-  }
-});
-    var snippetCode = md.render(snippet.snippetCode);
+    if(!snippet) {
+      return (
+        <div>no snippet found</div>
+      )
+    }
 
     return (
       <article className="snippet">
@@ -160,7 +179,7 @@ var Snippet = React.createClass({
                 src={require('!svg-inline!./images/tag.svg')}
               />
               <ul className="snippet__tags">
-                <li>CMS</li>
+                {snippetTags}
               </ul>
             </div>
             <div className="snippet__views">
@@ -168,20 +187,20 @@ var Snippet = React.createClass({
                 element="span"
                 src={require('!svg-inline!./images/eye.svg')}
               />
-              {snippet.views} views
+            {snippet.views + 1} views
             </div>
           </div>
           <div className="snippet__author">
-            Posted 5 days ago by <span className="snippet__author__name">Gil--</span>
+            Posted {moment(snippet.date).fromNow()} by <span className="snippet__author__name">{snippet.username}</span>
           </div>
 
-          {this.renderPagination()}
         </header>
         <div className="snippet__content">
           <div className="wrapper">
             <div dangerouslySetInnerHTML={{__html: snippetCode}} />
           </div>
         </div>
+        {this.renderPagination()}
       </article>
     )
   }
